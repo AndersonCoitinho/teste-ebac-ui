e5b1a1cbb217dec18d518316d36ac35424f00440
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-01-18 14:41:01 -0300

    Massa de dados em lista

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -12,6 +13,24 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Anderson', 'Coitinho', 'Google', 'Brasil', 'Rua XV', '560', 'Jaragua', 'Santa Catarina', '684521-55', '0616451616', 'emaiL@gamil.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+            dadosEndereco[2].telefone,
+            dadosEndereco[2].email
+            )
+    
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
