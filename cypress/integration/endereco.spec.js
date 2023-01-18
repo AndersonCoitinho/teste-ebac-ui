af76cccd4bce24b84cb1edefa1ae0a762832c04c
Anderson Coitinho <104800828+AndersonCoitinho@users.noreply.github.com>
2023-01-18 14:31:17 -0300

    Page Objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,6 +11,7 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+        EnderecoPage.editarEnderecoFaturamento('Anderson', 'Coitinho', 'Google', 'Brasil', 'Rua XV', '560', 'Jaragua', 'Santa Catarina', '684521-55', '0616451616', 'emaiL@gamil.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
